45b9c197c4ee0950568e6501f4f4e00b7e4ee79b
Lovish Jain <lovish888@Lovishs-MacBook-Air.local>
2024-05-31 18:02:56 +0530

    Added google sign in with temporary SignIn UI

--- src/screens/onboarding/Signup.tsx
+++ src/screens/onboarding/Signup.tsx
@@ -1,30 +1,109 @@
-import { authenticateUser } from '@api/user'
 import { AppText } from '@components/text/AppText'
-import { useMutation } from '@tanstack/react-query'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  User,
+} from '@react-native-google-signin/google-signin'
+import { AppColors } from '@utils/Colors'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { isEmpty } from '@utils/util'
+import { Image } from 'expo-image'
+import AppButton from '@components/AppButton'
+import { Spacer } from '@components/Spacer'
 
 export default function SignupScreen(): ReactElement {
-  const { mutate: signupUser, isPending } = useMutation({
-    mutationFn: authenticateUser,
-  })
+  const blurhash =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj['
 
-  const onSubmit = (): void => {
-    //signupUser()
+  useEffect(() => {
+    GoogleSignin.configure()
+  }, [])
+
+  const [user, setUser] = useState<User>()
+
+  const signIn = async (): Promise<void> => {
+    try {
+      GoogleSignin.hasPlayServices()
+      const currentUser = await GoogleSignin.signIn()
+      setUser(currentUser)
+      console.log('UserInfo - ', user)
+    } catch (e) {
+      console.log('Error - ', e)
+    }
+  }
+
+  const signOut = async (): Promise<void> => {
+    await GoogleSignin.revokeAccess()
+    await GoogleSignin.signOut()
+    setUser(undefined)
   }
 
   return (
-    <View style={styles.container}>
-      <AppText>Signup</AppText>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <AppText>Sign In!</AppText>
+        {!isEmpty(user) && (
+          <View style={styles.userContainer}>
+            <Image
+              style={styles.image}
+              source={user.user.photo}
+              placeholder={{ blurhash: blurhash }}
+              contentFit="contain"
+              transition={1000}
+            />
+            <View style={styles.userDetails}>
+              <AppText size="small">{user.user.name}</AppText>
+              <AppText size="small">{user.user.email}</AppText>
+            </View>
+          </View>
+        )}
+        {isEmpty(user) && (
+          <GoogleSigninButton
+            style={styles.googleSignIn}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Light}
+            onPress={signIn}
+            disabled={false}
+          />
+        )}
+        <Spacer vertical={32} />
+        {!isEmpty(user) && <AppButton label="Sign out" onPress={signOut} />}
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: AppColors.white,
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: 16,
+    paddingTop: 12,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  userContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 32,
+    paddingHorizontal: 32,
+  },
+  userDetails: {
+    paddingStart: 12,
+  },
+  image: {
+    width: 44,
+    height: 44,
+    backgroundColor: '#0553',
+  },
+  googleSignIn: {
+    height: 48,
+    borderColor: AppColors.border,
+    marginTop: 64,
   },
 })
