a58ec0d365dbf25b01cbdb6ce3c01390300486fc
Lovish Jain <lovish888@Lovishs-MacBook-Air.local>
2024-06-04 17:05:22 +0530

    Added expo enclave module for both android and ios

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -5,18 +5,30 @@ import { useNavigation } from '@react-navigation/native'
 import { Atoms } from '@state/Atoms'
 import { AppColors } from '@utils/Colors'
 import { useAtomValue } from 'jotai'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { getHardwareSecurityLevel } from '../../../modules/expo-enclave'
+import AppButton from '@components/AppButton'
+import { isEmpty } from '@utils/util'
+import { Spacer } from '@components/Spacer'
 
 export default function HomeScreen(): ReactElement {
-  const user = useAtomValue(Atoms.User)
   const mainNavigation = useNavigation()
+  const user = useAtomValue(Atoms.User)
+  const [securityLevel, setSecurityLevel] = useState('')
 
   function navigateToProfile(): void {
     mainNavigation.navigate('ProfileStack', {
       screen: 'Profile',
     })
+  }
+
+  // @danilo Test function to get hardware security level
+  async function getHardwareLevel(): Promise<void> {
+    const securityLevel = await getHardwareSecurityLevel()
+    setSecurityLevel(securityLevel)
+    console.log('Security - ', securityLevel)
   }
 
   return (
@@ -34,6 +46,12 @@ export default function HomeScreen(): ReactElement {
       />
       <View style={styles.content}>
         <AppText>Welcome to Zap Home!</AppText>
+        <Spacer vertical={24} />
+        {!isEmpty(securityLevel) && (
+          <AppText>Hardware security level - {securityLevel}</AppText>
+        )}
+        <Spacer vertical={12} />
+        <AppButton label="Check Security Level" onPress={getHardwareLevel} />
       </View>
     </SafeAreaView>
   )
