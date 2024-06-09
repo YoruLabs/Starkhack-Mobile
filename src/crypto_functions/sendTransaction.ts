5470cffce076223ee6d85e5f5d0239211a53ad8d
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>
2024-06-09 14:48:31 -0300

    x,y point generator

--- src/crypto_functions/sendTransaction.ts
+++ src/crypto_functions/sendTransaction.ts
@@ -1,10 +1,3 @@
-import {
-  fetchPublicKey,
-  createKeyPair,
-  sign,
-  verify,
-} from '../../../modules/expo-enclave'
-
 import { RpcProvider, Contract, Account } from 'starknet' // Import hash from starknet.js
 import { ABI } from '@utils/SignerConstants'
 import { EnclaveSigner } from '@utils/p256Signer'
@@ -21,30 +14,28 @@ let contract_address =
   '0x0400a92d5d009bca893d4699a3c06a389201a83ba56970b74ad8dfabfcb0d2e0'
 export async function createAndSignTransaction(): Promise<void> {
   try {
-    console.log('step 1')
     const enclaveSigner = new EnclaveSigner(pvt_key)
-    console.log('step 2')
 
     // 1. Create Account with EnclaveSigner
     const account = new Account(provider, account_address, enclaveSigner)
-    console.log('step 3')
 
     // Connect the new contract instance :
     const myTestContract = new Contract(ABI, contract_address, account)
-    console.log('myTestContract', myTestContract)
 
     // 4. Interaction with the contract read
     const count = await myTestContract.get()
+
     console.log('Initial count =', count)
 
     // 5. Execute contract call
-    const res = await myTestContract.set(10000000)
+    const res = await myTestContract.set(count + 100n)
 
     console.log(res.transaction_hash)
 
     await provider.waitForTransaction(res.transaction_hash)
 
     const count2 = await myTestContract.get()
+
     console.log('Final count =', count2)
   } catch (error) {
     console.error('Transaction error:', error)
--- src/screens/profile/Experiment.tsx
+++ src/screens/profile/Experiment.tsx
@@ -4,7 +4,6 @@ import { AppColors } from '@utils/Colors'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { StyleSheet, View } from 'react-native'
-import { fromBER } from 'asn1js'
 
 import {
   fetchPublicKey,
@@ -12,15 +11,13 @@ import {
   sign,
   verify,
 } from '../../../modules/expo-enclave'
-import { Buffer } from 'buffer'
 
-import { ABI, ACCOUNT_NAME, HEX_MESSAGE, PROMPT_COPY } from '@utils/SignerConstants'
+import { ACCOUNT_NAME, HEX_MESSAGE, PROMPT_COPY } from '@utils/SignerConstants'
 import Header from '@components/Header'
 import AppButton from '@components/AppButton'
 import { Spacer } from '@components/Spacer'
-// import { createAndSignTransaction } from './sendTransaction'
-import { Account, Contract, RpcProvider } from 'starknet'
-import { createAndSignTransaction } from './sendTransaction'
+import { createAndSignTransaction } from '../../crypto_functions/sendTransaction'
+import { derPublicKeyToXandY } from '@utils/crypto_utils'
 
 export default function ExperimentScreen(): ReactElement {
   const [message, setMessage] = useState('')
@@ -33,30 +30,14 @@ export default function ExperimentScreen(): ReactElement {
 
   async function handleFetchPublicKey(): Promise<void> {
     const publicKeyHex = await fetchPublicKey(ACCOUNT_NAME)
-    console.log('Public Key (Hex) - ', publicKeyHex)
+    let [x, y] = derPublicKeyToXandY(publicKeyHex)
 
-    setMessage(`Public Key Hex: (${publicKeyHex}`)
+    setMessage(`Public Key Hex: ${publicKeyHex} x: ${x} y: ${y}`)
   }
 
   async function handleSign(): Promise<void> {
-    console.log('------------------- Starting Sign -------------------')
     const signature = await sign(ACCOUNT_NAME, HEX_MESSAGE, PROMPT_COPY)
-    console.log('Signature (Hex) - ', signature)
-
-    const { r, s } = parseSignature(signature)
-    console.log('Signature (s) - ', s)
-    console.log('Signature (r) - ', r)
-
-    // Convert the hexadecimal message to Uint8Array
-    const hexMessage = HEX_MESSAGE
-    const messageBytes = hexMessage.match(/.{1,2}/g)?.map((byte) => parseInt(byte, 16))
-    console.log('Message (Uint8Array) - ', new Uint8Array(messageBytes!))
-
-    await handleFetchPublicKey()
-
     setSignature(signature)
-
-    // setSignature(`Signature (r, s): (${r}, ${s})`);
   }
 
   async function handleVerify(): Promise<void> {
@@ -71,71 +52,7 @@ export default function ExperimentScreen(): ReactElement {
     signature: any,
     HEX_MESSAGE: any,
   ): Promise<boolean> {
-    // Here we assume the ExpoEnclaveModule.verify function is correctly implemented and available
     return await verify(ACCOUNT_NAME, signature, HEX_MESSAGE)
-  }
-
-  // TODO: Fix this function to have the same result as "get_pubkey_point" from check_point.py
-  function parsePublicKey(publicKeyHex: string): { x: bigint; y: bigint } {
-    try {
-      console.log('Starting parsePublicKey function')
-
-      if (!publicKeyHex || typeof publicKeyHex !== 'string') {
-        throw new Error('Invalid public key input')
-      }
-
-      const publicKeyBuffer = Buffer.from(publicKeyHex, 'hex')
-
-      // Parse the DER-encoded public key
-      const asn1Data = fromBER(publicKeyBuffer)
-      const publicKeyInfo = asn1Data.result
-
-      const publicKeyBits = publicKeyInfo.valueBlock.value[1]
-
-      const xBytes = Buffer.from(
-        publicKeyBits.valueBlock.valueHexView.slice(
-          0,
-          publicKeyBits.valueBlock.valueHexView.length / 2,
-        ),
-        'hex',
-      )
-      const yBytes = Buffer.from(
-        publicKeyBits.valueBlock.valueHexView.slice(
-          publicKeyBits.valueBlock.valueHexView.length / 2,
-        ),
-        'hex',
-      )
-
-      const xBigInt = BigInt(`0x${xBytes.toString('hex')}`)
-      const yBigInt = BigInt(`0x${yBytes.toString('hex')}`)
-
-      console.log('Public Key (x): ', xBigInt.toString())
-      console.log('Public Key (y): ', yBigInt.toString())
-
-      return { x: xBigInt, y: yBigInt }
-    } catch (error) {
-      console.error('Error in parsePublicKey function: ', error)
-      throw error
-    }
-  }
-  function parseSignature(signature): { r: bigint; s: bigint } {
-    const signatureBuffer = Buffer.from(signature, 'hex')
-    const signatureBytes = new Uint8Array(signatureBuffer)
-
-    // Assume the signature is in the DER format (0x30 || length || 0x02 || r_length || r || 0x02 || s_length || s)
-    const rLength = signatureBytes[3]
-    const r = signatureBytes
-      .slice(4, 4 + rLength)
-      .reduce((acc, val) => (acc << 8n) + BigInt(val), 0n)
-
-    const sLength = signatureBytes[4 + rLength + 1]
-    const sStart = 4 + rLength + 2
-    const sEnd = sStart + sLength
-    const s = signatureBytes
-      .slice(sStart, sEnd)
-      .reduce((acc, val) => (acc << 8n) + BigInt(val), 0n)
-
-    return { r, s }
   }
 
   return (
