b40c99ce6052508b01a1bf83da282b1f6c879eaf
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>
2024-06-20 20:37:54 -0300

    implement send_transaction

--- src/screens/home/PreviewSend.tsx
+++ src/screens/home/PreviewSend.tsx
@@ -11,6 +11,12 @@ import Strings from '@utils/Strings'
 import { Spacer } from '@components/Spacer'
 import { Card } from '@components/Card'
 import ViewFiller from '@components/ViewFiller'
+import ERC20Manager from '@utils/crypto/ERC20Manager'
+import {
+  ACCOUNT_ADDRESS,
+  ERC20_ADDRESS,
+  RPC_ENDPOINT,
+} from '@utils/crypto/SignerConstants'
 
 export default function PreviewSendScreen({
   navigation,
@@ -21,17 +27,44 @@ export default function PreviewSendScreen({
 
   const [isLoading, setLoading] = useState(false)
 
-  function onSendPress(): void {
-    // TODO: Call the backebnd send function
+  async function onSendPress(): Promise<void> {
+    // TODO: Get account address from Global State
+    let accountAddress = ACCOUNT_ADDRESS
+    let pvt_key = '0x2bbf4f9fd0bbb2e60b0316c1fe0b76cf7a4d0198bd493ced9b8df2a3a24d68a'
+
+    // Get the recipient email, amount, and currency from the route params
+    const { recipientEmail, amount, currency } = details
+
+    console.log('Recipient Email:', recipientEmail)
+    console.log('Token:', currency.code)
+    console.log('Amount:', amount)
+
+    // TODO: FETCH ACCOUNT_ADDRESS FROM BACKEND
+    let to = '0x01f7888e80ef310fc98d8e82b9e2f22cf962ee0342fe830aaabeaf1b0fc05bdf'
+    // TODO: GET ERC20_ADDRESS based on "currency.code" or "currency.address"
+    const erc20Manager = new ERC20Manager(
+      accountAddress,
+      ERC20_ADDRESS,
+      RPC_ENDPOINT,
+      pvt_key,
+    )
+
     setLoading(true)
-    setTimeout(() => {
+
+    try {
+      const txHash = await erc20Manager.transfer(to, amount) // Mint 1000 tokens
+      console.log('txHash', txHash)
+
       setLoading(false)
       addToast({
         message: Strings.SEND_SUCCESS,
         type: 'success',
       })
       navigation.navigate('Home')
-    }, 2000)
+    } catch (error) {
+      console.error('Transaction error:', error)
+      setLoading(false)
+    }
   }
 
   return (
--- src/utils/crypto/SignerConstants.ts
+++ src/utils/crypto/SignerConstants.ts
@@ -8,11 +8,11 @@ export const PROMPT_COPY: PromptCopy = {
   androidTitle: 'Authentication Required',
 }
 export const ERC20_ADDRESS: string =
-  '0x011d777726de0a5b99fa57f4056efc9ec63da48ddd5e49275253fce12c2bd627'
+  '0x0641a7e12c43701d4b437468b5047a2e748e5f6a7f562395262cf6f60645d397'
 export const ACCOUNT_ADDRESS: string =
-  '0x15ed3e955161432ff55d43717e8c44c7ee4cce8dff91f10d1833969909e3d86'
+  '0x14d781e241ef4edf92e231d50407c5e6cd03d9922e0abe6f4c271607a140f74'
 
-export const RPC_ENDPOINT = 'https://4fc9-189-120-76-5.ngrok-free.app'
+export const RPC_ENDPOINT = 'https://0d84-189-120-76-5.ngrok-free.app'
 
 export const ABI = [
   {
