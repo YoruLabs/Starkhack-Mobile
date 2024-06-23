2fa4fe4e358a9812155bb8309f619207373340a5
Lovish Jain <lovish888@Lovishs-MacBook-Air.local>
2024-06-23 12:44:41 +0530

    removed files

--- src/utils/Credentials.ts
+++
@@ -1,3 +0,0 @@
-const STRAVA_CLIENT_ID = '[CLIEND-ID]'
-export const REDIRECT_URI = 'zap-mobile://zap-mobile/callback'
-export const STRAVA_AUTH_URL = `https://www.strava.com/oauth/authorize?client_id=${STRAVA_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=activity:read_all`
